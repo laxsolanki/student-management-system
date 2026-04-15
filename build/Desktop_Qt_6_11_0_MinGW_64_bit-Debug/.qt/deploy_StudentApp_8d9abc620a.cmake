d73f1276e7820a4824c238902864fdebaa8a65ab
Lalit Kumar <tumhara_github_lalitksolanki123@gmail.com>
2026-04-15 15:27:08 +0530

    removed build files

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/deploy_StudentApp_8d9abc620a.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/HP/OneDrive/Desktop/QtProjects/StudentApp/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/StudentApp-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/HP/OneDrive/Desktop/QtProjects/StudentApp/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/StudentApp.exe"
-    GENERATE_QT_CONF
-)
