f648bb79f3adc1cfec05ebdcb6da7e96e5281afd
Shubhamiitj <b25me1076@iitj.ac.in>
2026-04-20 00:51:29 +0530

    Update login.cpp

--- login.cpp
+++ login.cpp
@@ -7,7 +7,6 @@ Login::Login(QWidget *parent)
     : QDialog(parent), ui(new Ui::Login)
 {
     ui->setupUi(this);
-
     connect(ui->usernameEdit, &QLineEdit::returnPressed,
             this, [=]()
             {
@@ -16,41 +15,32 @@ Login::Login(QWidget *parent)
     connect(ui->passwordEdit, &QLineEdit::returnPressed,
             this, &Login::on_Login_clicked);
 }
-
-Login::~Login()
-{
+Login::~Login(){
     delete ui;
 }
 
-void Login::on_Login_clicked()
-{
+void Login::on_Login_clicked(){
     QString username = ui->usernameEdit->text().trimmed().toLower();
     QString password = ui->passwordEdit->text();
 
-    // ❗ password empty → kuch nahi
     if(password.isEmpty()){
         return;
     }
 
-    // ❗ username empty → focus wapas
     if(username.isEmpty()){
         ui->usernameEdit->setFocus();
         return;
     }
 
-    // ✅ check
-    if(username == "admin" && password == "1007")
-    {
-        accept(); // no popup
+    if(username == "admin" && password == "1007"){
+        accept(); 
     }
-    else
-    {
+    else{
         QMessageBox::warning(this, "Error", "Invalid Username or Password");
         ui->passwordEdit->clear();
         ui->passwordEdit->setFocus();
     }
 }
-void Login::on_loginButton_clicked()
-{
-    on_Login_clicked();  // 👈 bas ye hi
+void Login::on_loginButton_clicked(){
+    on_Login_clicked();
 }
